d466c791f87f738f563d792d656dd250d2ea97ca
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2024-01-24 20:14:45 +0700

    chat

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
 export class MessagesService {
+  message: Message[] = [
+    {
+      name: 'Peeratchia',
+      text: 'Hello',
+    },
+  ];
+
+  clientToUser = {};
+
+  identify(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+    console.log(this.clientToUser);
+
+    return Object.values(this.clientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+
   create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+    const message = { ...createMessageDto };
+    this.message.push(createMessageDto); //TODO: improve
+    return message;
   }
 
   findAll() {
-    return `This action returns all messages`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} message`;
-  }
-
-  update(id: number, updateMessageDto: UpdateMessageDto) {
-    return `This action updates a #${id} message`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} message`;
+    return this.message;
   }
 }
